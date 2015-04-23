6b1a5e279aff89ef49e6a40015aa948ecb23de8c
Adriann Felipe Sanchez Sierra <alka65@hotmail.com>
2015-04-23 10:20:38 -0500

    Arreglos en habilidades y maquetacion busqueda

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'app.views.inicio',name='inicio'),
+    url(r'^Buscar/', 'app.views.find',name='find'),
 )
