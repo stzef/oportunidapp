273fbf27ac72446e2c37f757f78439ccfd1c9242
Juan Sandoval <juancsandoval86@gmail.com>
2015-06-18 08:28:34 -0500

    mensaje te necesito al usuario

--- necesito/views.py
+++ necesito/views.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 
 
 from habilidades.models import habilidadesModel
-from necesito.forms import teNecesitoForm
 from usuarios.models import perfilUsuarioModel
+from necesito.models import teNecesitoModel
 
 # Create your views here.
 def necesito(request):
 	return render(request,'necesito.html')
 
-#@login_required()
+#@login_required()	
 def teNecesito(request, usuarioSolicitado, habilidadSlug):
 	usuario = get_object_or_404(User, username=usuarioSolicitado)
 	perfil = perfilUsuarioModel.objects.get(usuario = usuario)
@@ -27,4 +27,20 @@ def teNecesito(request, usuarioSolicitado, habilidadSlug):
 	return render(request,'te_necesito.html', context)
 
 def crearMensajeSolicitud(request):
-	return HttpResponse(request)
+	if request.method == "POST":
+		usuarioSolicitante = perfilUsuarioModel.objects.get(usuario=request.user.id)
+		usuarioRequerido = perfilUsuarioModel.objects.get(usuario=request.POST['usuarioRequerido'])
+		habilidadSolicitada = habilidadesModel.objects.get(id=request.POST['habilidadSolicitada'])
+		mensaje = request.POST['mensaje']
+
+		necesitoSolicitud = teNecesitoModel(
+			usuarioSolicitante = usuarioSolicitante,
+			usuarioRequerido = usuarioRequerido,
+			habilidadSolicitada = habilidadSolicitada,
+			mensaje = mensaje,
+		)
+
+		necesitoSolicitud.save()
+
+		return HttpResponse(necesitoSolicitud)
+
