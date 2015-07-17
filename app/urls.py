a03b72efe65adbeb8b2ae0516c4fc0556257da5e
carlosturnerbenites <carlosturnerbenites@gmail.com>
2015-07-17 07:56:37 -0500

    Formulario de preguntas responsive y trabajo sobre imgagenes

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'app.views.inicio',name='inicio'),
-    url(r'^404/$', 'app.views.view_404',name='404'),
-    url(r'^500/$', 'app.views.view_500',name='500'),
+	url(r'^$', 'app.views.inicio',name='inicio'),
+	url(r'^404/$', 'app.views.view_404',name='404'),
+	url(r'^500/$', 'app.views.view_500',name='500'),
 	#url(r'^resultados/detalle/$', 'app.views.resultadosDetalle',name='resultados-detalle'),
-)
+	)
